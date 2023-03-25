cdf80aa09bca44e283e596396d83b6c66f991a0f
Anish Anam <97582787+AnishAnamProjects@users.noreply.github.com>
2023-03-24 23:13:30 -0600

    Added Basic UI

--- app/src/main/java/com/example/walkwarriors/MainActivity.java
+++ app/src/main/java/com/example/walkwarriors/MainActivity.java
@@ -1,19 +1,20 @@
 package com.example.walkwarriors;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.TextView;
 
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
    private TextView text;
+   private ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = findViewById(R.id.Steps);
-        String texto = "yolo";
-        text.setText(texto);
+        text = findViewById(R.id.Health);
+        String texto = "yolo2";
     }
 }
